49e0c5664f920e4f00db60fd920d5bfe37672def
Risko Ruus <risko.ruus@gmail.com>
2012-03-14 13:25:41 +0200

    v0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musiccollection::Application.config.secret_token = 'c3d21b0253060e39a06454d0ae073718845b238fa8421509ac54044e0986f7781aaedf8fd2632886a0ff6ac54e6ead4b424ed50f86e6175d517f48b82333e9cb'
